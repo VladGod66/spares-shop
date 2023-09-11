c8a91dcab26738833247ba7edf9bfd78fcf48637
Vlad <vladgod@mail.ru>
2023-09-11 22:07:33 +0300

    Добавляет секцию промо

---
+++ source/js/counter.js
@@ -0,0 +1,20 @@
+const countdown = document.querySelector('.countdown');
+const targetDate = new Date('2023-09-31T00:00:00');
+
+function updateCountdown() {
+  const now = new Date();
+  const remainingTime = targetDate - now;
+
+  const days = Math.floor(remainingTime / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((remainingTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((remainingTime % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((remainingTime % (1000 * 60)) / 1000);
+
+  document.getElementById('days').innerText = days.toString().padStart(2, '0');
+  document.getElementById('hours').innerText = hours.toString().padStart(2, '0');
+  document.getElementById('minutes').innerText = minutes.toString().padStart(2, '0');
+  document.getElementById('seconds').innerText = seconds.toString().padStart(2, '0');
+}
+
+// Обновляем счетчик каждую секунду
+setInterval(updateCountdown, 1000);
