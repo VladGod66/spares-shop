332d7eff46c207ab87a81fe0b72e473a94a94e82
Vlad <vladgod@mail.ru>
2023-09-14 20:51:29 +0300

    Добавляет слайдеру все функции

--- source/js/slider.js
+++ source/js/slider.js
@@ -6,28 +6,43 @@ const sliderImages = document.querySelector('.slider__images');
 const prevButton = document.querySelector('.slider__prev-button');
 //Находим кнопку вперёд
 const nextButton = document.querySelector('.slider__next-button');
+//Создаём массивоподобную коллекцию классов точек индикатора
+const dots = document.querySelectorAll('.slider__dot');
 //Создаём массив из найденных в контейнере слайдера изображений
 const slides = Array.from(sliderImages.querySelectorAll('img'));
 //Определяем количество изображений в массиве
 const slideCount = slides.length;
+//Определяем интервал автоматического перелистывания слайдов
+const interval = 10000;
 //Создаём индекс активного слайда
 let slideIndex = 0;
+//Создаём индекс активной точки индикатора
+let dotIndex = 0;
+
 
 //При клике на кнопку назад
 prevButton.addEventListener('click', () => {
   //Пересчитываем индекс нового активного слайда от 0 до lenght-1
   slideIndex = slideIndex - 1;
   //Если слайд первый
-  if (slideIndex <= 0) {
+  if (slideIndex < 0) {
     //То переходим к последнему слайду
     slideIndex=slideCount-1;
   }
-  //Перезапускаем функцию slide
+  dotIndex = slideIndex;
+  //Перезапускаем функцию slide для сдвига флекса массива изображений
   slide();
+  //Перезапускаем функцию thisSlide с текущим индексом псевдомассива для смены активной точки индикатора
+  thisSlide(dotIndex);
 });
 
 //При клике на кнопку вперёд
 nextButton.addEventListener('click', () => {
+  nextSlide()
+});
+
+//Функция перемещения слайдера на один слайд вперёд
+const nextSlide = () => {
   //Пересчитываем индекс нового активного слайда от 0 до lenght-1
   slideIndex = slideIndex + 1;
   //Если слайд последний
@@ -35,9 +50,38 @@ nextButton.addEventListener('click', () => {
     //То переходим к первому слайду
     slideIndex=0;
   }
-  //Перезапускаем функцию slide
+  dotIndex = slideIndex;
+  //Перезапускаем функцию slide для сдвига флекса массива изображений
   slide();
-});
+  //Перезапускаем функцию thisSlide с текущим индексом псевдомассива для смены активной точки индикатора
+  thisSlide(dotIndex);
+}
+
+//Колбэк функция переключения слайдера на выбранную позицию в индикаторе
+//Для каждого элемента массивоподобной коллекции точек индикатора
+dots.forEach((slider__dot, index) => {
+  //При клике на любую точку индикатора получаем индекс кликнутой точки
+  slider__dot.addEventListener('click', () => {
+    //Присваеваем индексу слайда индекс кликнутой точки
+    slideIndex = index;
+    dotIndex = slideIndex;
+    //Перезапускаем функцию slide для сдвига флекса массива изображений
+    slide();
+    //Перезапускаем функцию thisSlide с текущим индексом псевдомассива для смены активной точки индикатора
+    thisSlide(dotIndex);
+  })
+})
+
+//Функция смены активной точки индикатора
+const thisSlide = (index) => {
+  //Для каждого класса slider__dot в псевдомассиве dots
+  for (let slider__dot of dots) {
+    //Удаляем класс активной точки индикатора
+    slider__dot.classList.remove('slider__dot--active')
+  }
+  //Добавляем класс активной точки индикатора классу с заданным индексом в псевдомассиве
+  dots[index].classList.add('slider__dot--active')
+}
 
 //Функция перемещения флекса массива изображений в окне просмотра
 const slide = () => {
@@ -53,3 +97,6 @@ const slide = () => {
 window.addEventListener('load', () => {
   slide();
 });
+
+//Функция автоматического перелистывания слайдов
+setInterval(() => {nextSlide()}, interval)
